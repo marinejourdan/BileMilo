5a35c55210ea5439a5c133590dfdfc61784fcf5a
mjourdan <marine.misser@gmail.com>
2022-09-12 15:01:16 +0200

    #7 first cache cith get phones

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -10,17 +10,25 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class PhoneController extends AbstractController
 {
     #[Route('/api/phones', name: 'app_phone',methods:['GET'])]
-    public function getAllPhones(PhoneRepository $phoneRepository, SerializerInterface $serializer, Request $request): JsonResponse
+    public function getAllPhones(PhoneRepository $phoneRepository, SerializerInterface $serializer, Request $request, TagAwareCacheInterface $cache): JsonResponse
     {
         $page=$request->get('page',1);
         $limit=$request->get('limit', 2);
-        $phoneList = $phoneRepository->findAllWithPagination($page, $limit);
-        $jsonPhoneList=$serializer->serialize($phoneList,'json');
 
+        $idCache="getAllPhones".$page."-".$limit;
+        $phoneList=$cache->get($idCache, function (ItemInterface $item) use ($phoneRepository, $page, $limit){
+            echo ("l'element n'est pas encore en cache");
+            $item->tag("phonesCache");
+            return $phoneRepository->findAllWithPagination($page, $limit);
+        });
+
+        $jsonPhoneList=$serializer->serialize($phoneList,'json');
         return new JsonResponse($jsonPhoneList, Response::HTTP_OK,[],true);
     }
 
